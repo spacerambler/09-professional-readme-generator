066b0ae2cee6d818ba86702e03b21fe60741010d
Keely Bailey <keelybailey3@gmail.com>
2021-08-25 14:25:28 -0500

    Move lines of code around

--- 02-Homework/Develop/utils/generateMarkdown.js
+++ 02-Homework/Develop/utils/generateMarkdown.js
@@ -12,7 +12,40 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${answers.title}
+
+## Table of Contents
+*[Description](#header-description)
+*[Installation](#header-installation)
+*[Usage](#header-usage)
+*[License](#header-license)
+*[Contributing](#header-contributing)
+*[Tests](#header-tests)
+*[Questions](#header-questions)
+
+## <a id="header-description"></a>Description
+${answers.description}
+
+## <a id="header-installation"></a>Installation
+${answers.installation-instructions}
+
+## <a id="header-usage"></a>Usage
+${answers.usage-information}
+
+## <a id="header-license"></a>License
+${answers.license}
+
+## <a id="header-contributing"></a>Contributing
+${answers.contribution-guidelines}
+
+## <a id="header-tests"></a>Tests
+${answers.test-instructions}
+
+## <a id="header-questions"></a>Questions
+
+[Check me out on Github](https://github.com/${answers.github-username})
+[Email Me](mailto:${answers.email-address})
 
 `;
 }
