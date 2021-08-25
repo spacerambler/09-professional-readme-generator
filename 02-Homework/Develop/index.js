c5452df90fc177786beb8d80d364a51adb33c144
Keely Bailey <keelybailey3@gmail.com>
2021-08-25 13:43:56 -0500

    Start write functions

--- 02-Homework/Develop/index.js
+++ 02-Homework/Develop/index.js
@@ -1,6 +1,9 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer')
 const fs = require('fs')
+const util = require('util');
+
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -52,10 +55,19 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(README, data) {
+    `
+    #${}
+    `
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    promptUser()
+    .then((answers) => writeFileAsync('README.txt', writeToFile(answers)))
+    .then(() => console.log('Successfully wrote to README.txt'))
+    .catch((err) => console.error(err));
+}
 
 // Function call to initialize app
 init();
